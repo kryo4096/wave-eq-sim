41e6c15d719f4a01eea404f66c0ce9c38019b753
Jonas Luther <kryo4096@gmail.com>
2021-03-26 12:34:54 +0100

    simple compute shader example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use std::sync::Arc;
+
 use vulkano::{
     buffer::{BufferUsage, CpuAccessibleBuffer},
     command_buffer::{AutoCommandBufferBuilder, CommandBuffer},
+    descriptor::{descriptor_set::PersistentDescriptorSet, PipelineLayoutAbstract},
     device::{Device, DeviceExtensions, Features},
     instance::{Instance, InstanceExtensions, PhysicalDevice, QueueFamily},
+    pipeline::{shader::SpecializationConstants, ComputePipeline},
     sync::GpuFuture,
 };
+
+mod cs {
+    vulkano_shaders::shader! {
+        ty: "compute",
+        path: "res/shaders/compute.glsl"
+    }
+}
 
 fn main() {
     let instance =
@@ -22,29 +33,46 @@ fn main() {
     let (device, mut queues) = Device::new(
         physical,
         &Features::none(),
-        &DeviceExtensions::none(),
+        &DeviceExtensions {
+            khr_storage_buffer_storage_class: true,
+            ..DeviceExtensions::none()
+        },
         std::array::IntoIter::new([(queue_family, 0.5)]),
     )
     .expect("failed to create device");
 
     let queue = queues.next().unwrap();
 
-    let source_buffer =
-        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, 0..64)
+    let data_buffer =
+        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, 0..65536)
             .expect("failed to create buffer");
 
-    let dest_buffer = CpuAccessibleBuffer::from_iter(
-        device.clone(),
-        BufferUsage::all(),
-        false,
-        (0..64).map(|_| 0),
-    )
-    .expect("failed to create buffer");
+    let shader = cs::Shader::load(device.clone()).expect("failed to create shader");
+
+    let compute_pipeline = Arc::new(
+        ComputePipeline::new(device.clone(), &shader.main_entry_point(), &(), None)
+            .expect("failed to create compute pipeline"),
+    );
+
+    let layout = compute_pipeline.layout().descriptor_set_layout(0).unwrap();
+
+    let set = Arc::new(
+        PersistentDescriptorSet::start(layout.clone())
+            .add_buffer(data_buffer.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
 
     let mut builder = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap();
-
     builder
-        .copy_buffer(source_buffer.clone(), dest_buffer.clone())
+        .dispatch(
+            [1024, 1, 1],
+            compute_pipeline.clone(),
+            set.clone(),
+            (),
+            None,
+        )
         .unwrap();
 
     let command_buffer = builder.build().unwrap();
@@ -57,7 +85,10 @@ fn main() {
         .wait(None)
         .unwrap();
 
-    let src_content = source_buffer.read().unwrap();
-    let dest_content = dest_buffer.read().unwrap();
-    assert_eq!(&*src_content, &*dest_content);
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        assert_eq!(*val, n as u32 * 12);
+    }
+
+    println!("Everything succeeded!");
 }
